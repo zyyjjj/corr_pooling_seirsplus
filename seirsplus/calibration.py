96dd0fd3a0e12ece87bc705ac9934d3597234d7a
zyyjjj <yz685@cornell.edu>
2023-05-27 21:14:18 -0400

    (in progress) implement VL model with plateau

--- seirsplus/calibration.py
+++ seirsplus/calibration.py
@@ -61,3 +61,101 @@ def gen_vl_distrbution(
         + viral_loads
     )
     return (viral_loads, time_stamps, peaks)
+
+
+# TODO: later we can adapt this model to update the VL mechanism in ViralModel
+def _get_vl_with_plateau(
+    critical_time_points: List[float],
+    peak_plateau_height: float,
+    tail_plateau_height: float,
+    sample_time: float,
+):
+    r"""Sample one log10 VL at a given time under the VL-with-plateau model.
+    
+    Args:
+        critical_time_points: A list of 4 floats reprensenting the time points 
+            at which VL starts the peak plateau, starts decaying from the peak plateau, 
+            enters the tail plateau, and drops to 0 from the tail plateau. # TODO: double check
+        peak_plateau_height: A float representing the height of the peak plateau.
+        tail_plateau_height: A float representing the height of the tail plateau.
+        sample_time: A float representing the time at which to sample the VL.
+    Returns:
+        A float representing the log10 VL at the given time.
+    """
+
+    start_peak_time, start_decay_time, start_tail_time, end_tail_time = critical_time_points
+
+    vl = 0
+
+    if sample_time < start_peak_time:
+        vl = peak_plateau_height / start_peak_time * sample_time
+    elif sample_time < start_decay_time:
+        vl = peak_plateau_height
+    elif sample_time < start_tail_time:
+        vl = peak_plateau_height - (peak_plateau_height - tail_plateau_height) / (start_tail_time - start_decay_time) * (sample_time - start_decay_time)
+    elif sample_time < end_tail_time:
+        vl = tail_plateau_height
+    else:
+        vl = 0
+    
+    return vl
+
+
+def gen_vl_distribution_with_plateau(
+    critical_time_points_bounds: List[Tuple[float]],
+    peak_plateau_height_bounds: Tuple[float], # TODO: keep this fixed? or vary
+    tail_plateau_height: float,
+    tail_plateau_duration: float, # TODO: double check
+    num_samples: int,
+):
+    r"""Generate log10 viral load samples from a piecewise linear model with plateau.
+
+    Args:
+        critical_time_points_bounds: A 4-element list of tuples of the form 
+            (min, max) for each time point at which VL starts the peak plateau, 
+            starts decaying from the peak plateau, enters the tail plateau, 
+            and drops to 0 from the tail plateau. 
+        peak_plateau_height_bounds: A tuple of the form (min, max) for the
+            height of the peak plateau.
+        tail_plateau_height: A float representing the height of the tail plateau.
+        num_samples: The number of log10 viral load samples to generate.
+
+    Returns:
+        A 1-d array of size `num_samples` representing the log10 viral load samples.
+    """
+
+
+    # sample critical time points
+    critical_time_points_l = [
+        np.random.uniform(
+        critical_time_points_bounds[i][0], 
+        critical_time_points_bounds[i][1],
+        (num_samples, 1)
+        )
+        for i in range(len(critical_time_points_bounds))
+    ]
+    critical_time_points_l = np.concatenate(critical_time_points_l, axis=1)  # shape: (num_samples, 3)
+
+    # sample the peak plateau heights
+    peak_plateau_height_l = np.random.uniform(
+        peak_plateau_height_bounds[0], 
+        peak_plateau_height_bounds[1], 
+        (num_samples, 1)
+    )
+
+    time_stamps = np.random.uniform(0, critical_time_points_l[:, -1], num_samples)
+
+    sampled_vls = []
+
+    for i in range(num_samples):
+        _vl = _get_vl_with_plateau(
+            critical_time_points_l[i], 
+            peak_plateau_height_l[i], 
+            tail_plateau_height,
+            time_stamps[i]
+        )
+        sampled_vls.append(_vl)
+    
+    return sampled_vls, time_stamps
+
+
